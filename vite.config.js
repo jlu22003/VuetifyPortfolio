71a929e1ee78eabb0c10fe27d3e407221569de3c
Justin Kahshen Lu <justinklu75@gmail.com>
2023-09-08 20:15:18 -0400

    testing build

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VuetifyPortfolio/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
